c32a05b1c97df5e3622fe1170a957ce2dc79294f
Zeetabi <zaishazabi21@gmail.com>
2025-06-23 15:37:06 +0000

    1_variables.js

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+// alert("hello"); we can not use alert as it this since its nodejs not browser
+
+console.log(3+3); console.log("hii")// not readeable not good practice
+
+let name = "zeet" //string
+let age = 32      //number
+let isLoggedIn = false    //boolean
+
+//null => standalone value
+//undefined => value not assined
+//null is value is something that is not undefined like its empty
+// Symbols => unique , to define uniqueness
+
+//objects 
+console.log(typeof age); //opt = number
+console.log(typeof null); //opt = object
+console.log(typeof undefined);// opt = undefined
+
+
