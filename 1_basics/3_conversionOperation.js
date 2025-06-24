1405a1d7088143132a337513cf819fcbd6109e59
Zeetabi <zaishazabi21@gmail.com>
2025-06-24 11:38:08 +0000

    conversion operation

--- 1_basics/3_conversionOperation.js
+++ 1_basics/3_conversionOperation.js
@@ -1,4 +1,27 @@
-let score = 33 
+let score = true
  
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score) // Number is a datatype we are doing type conversion
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);// opt Nan is a value 
+// "33" => 33
+// "33bc"=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = "";
+let booleanIsLoggedIN = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIN);
+//   1 = true; 0 => false
+// "" => false
+//  "hii" => true
+
+let someNumber = 33
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+ // converted to string
+
+ 
