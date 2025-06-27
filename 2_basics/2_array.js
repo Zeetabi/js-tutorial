a73b6522d5bba9eb9bda439134eb6df1237c95bf
Zeetabi <zaishazabi21@gmail.com>
2025-06-27 16:25:54 +0000

    changes in array2

--- 2_basics/2_array.js
+++ 2_basics/2_array.js
@@ -9,10 +9,24 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(allHero);//now all arrays merged into one
 //[ 'thor', 'ironMan', 'spiderman', 'superman', 'flash', 'batman' ]
 
-const all_new_heros = [...marvel_heros, ...dc_heros]//... spread operator to merge arrays
-console.log(all_new_heros);//[ 'thor', 'ironMan', 'spiderman', 'superman', 'flash', 'batman' ]
+// const all_new_heros = [...marvel_heros, ...dc_heros]//... spread operator to merge arrays
+// console.log(all_new_heros);//[ 'thor', 'ironMan', 'spiderman', 'superman', 'flash', 'batman' ]
 
-const another_array = [1,2,3, [4,5,6],7,[8,9,[1,2]]]
+// const another_array = [1,2,3, [4,5,6],7,[8,9,[1,2]]]
 
-const real_another_array = another_array.flat(Infinity);//flat() spread all arrays elements 
-console.log(real_another_array);
+// const real_another_array = another_array.flat(Infinity);//flat() spread all arrays elements 
+// console.log(real_another_array);
+
+console.log(Array.isArray("hiit"));//tells whether it is array or or like boolean value
+console.log(Array.from("hiit"));//convert array from given value
+console.log(Array.from({name: "hiit"}));//return empty []
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));//converts into arrays ,creates arrays from values
+
+
+
+
